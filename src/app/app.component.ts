ffef0b6f25c8c003640780a3714e88a25682a2fd
Bostjan <bostjanzup@gmail.com>
2021-12-29 19:50:17 +0100

    two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { StorageService } from './storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'crash-course-mirza';
+
+  handleEvent () {
+    console.log("Button Clicked", this.title);
+
+  }
 }
